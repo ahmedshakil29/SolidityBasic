a42cd7e800266629d7177b8a8db5c0c0d670053b
shakil ahmed <ahmedshakil.aiub@gmail.com>
2023-01-21 14:23:58 +0600

    1st

---
+++ constructor.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+
+// condtructor start
+ contract local
+ {
+     uint public  count;
+
+//without argument start
+constructor()
+{
+    count= 8;
+} 
+//without argument end
+
+//with argument start
+// constructor(uint newCount)
+// {
+//     count= newCount;
+// } 
+
+//with argument end
+ }
---
+++ local Variable.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+// local variable start
+
+ contract local 
+{
+    function store() pure public returns(uint)
+    {
+        string memory name ="BRO";
+        uint age = 10;
+        return age;
+    }
+}
+
+// local variable end
